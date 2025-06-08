0f1d050a2ad1dbd71336c0c8978653b513221e13
DhamariTH <dhamaritricehanson@gmail.com>
2025-06-08 15:51:18 -0400

    Addded Navigating

--- frontend/NoMab_App/app/_layout.jsx
+++ frontend/NoMab_App/app/_layout.jsx
@@ -1,13 +1,18 @@
 import {Stack} from "expo-router";
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import Home from "./(tabs)/Home";
+import Loading from "./screens/loading";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
 export default function RootLayout() {
+    const Stack = createNativeStackNavigator();
     return (
         <SafeAreaProvider>
             <SafeAreaView style={{flex: 1}}>
-                <Stack>
-                    <Stack.Screen name="index" options={{headerShown: false}}/>
-                </Stack>
+                <Stack.Navigator>
+                    <Stack.Screen name="Loading" component={Loading} options={{headerShown: false}}/>
+                    <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
+                </Stack.Navigator>
             </SafeAreaView>
         </SafeAreaProvider>
     );
