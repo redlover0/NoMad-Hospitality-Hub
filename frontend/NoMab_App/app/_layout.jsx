4321a2371de0f31629490644d55153f389a248bd
DhamariTH <dhamaritricehanson@gmail.com>
2025-06-12 22:09:20 -0400

    added new route, RoomMood

--- frontend/NoMab_App/app/_layout.jsx
+++ frontend/NoMab_App/app/_layout.jsx
@@ -1,12 +1,12 @@
 import {Stack} from "expo-router";
-import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import Home from "./Home";
 import Loading from "./loading";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {View, StyleSheet} from "react-native";
+import { StyleSheet} from "react-native";
 import TabLayout from "./(tabs)/index";
 import Room from "./room_service";
 import AboutUs from "./aboutUs";
+import RoomMood from "./roomMood";
 
 export default function RootLayout() {
     const Stack = createNativeStackNavigator();
@@ -14,10 +14,21 @@ export default function RootLayout() {
         <Stack.Navigator>
             <Stack.Screen name="Loading" component={Loading}
                           options={{headerShown: false, presentation: 'modal', animation: 'slide_from_bottom'}}/>
-            <Stack.Screen name="home" component={Home} options={{headerShown: false,title: 'Home' }}/>
-            <Stack.Screen name="tab" component={TabLayout} options={{headerShown: false}}/>
-            <Stack.Screen name="room_service" component={Room} options={{headerShown: false, title: 'Room Service', animation: 'slide_from_bottom', }}/>
-            <Stack.Screen name="aboutUs" component={AboutUs} options={{headerShown: false, title: 'About Us', animation: 'slide_from_right',}}/>
+
+            <Stack.Screen name="home"
+                          component={Home} options={{headerShown: false,title: 'Home' }}/>
+
+            <Stack.Screen name="tab"
+                          component={TabLayout} options={{headerShown: false}}/>
+
+            <Stack.Screen name="room_service"
+                          component={Room} options={{headerShown: false, title: 'Room Service', animation: 'slide_from_bottom', }}/>
+
+            <Stack.Screen name="aboutUs"
+                          component={AboutUs} options={{headerShown: false, title: 'About Us', animation: 'slide_from_right',}}/>
+
+            <Stack.Screen name="roomMood"
+                          component={RoomMood} options={{headerShown: false, title: 'Room Mood', animation: 'slide_from_bottom', }}/>
         </Stack.Navigator>
     );
 }
